a4fc8487e1af0c7b78ef855b567116abc975f188
Lott Lalime <lalime@mail.usf.edu>
2020-02-26 16:50:22 -0500

    added forms

---
+++ Week 5/index.js
@@ -0,0 +1,163 @@
+//user's rock paper scissors choice
+var user_choice = null;
+
+//user's score
+var user_score = 0;
+
+//system's rock ppaer scissors choice
+var system_choice = null;
+
+//system's score
+var system_score = 0;
+
+//array to pull random choice from
+var rps = ["rock", "paper", "scissors"];
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    /*Rock button handler*/
+    document.getElementById("Rock").addEventListener('click', function ()
+    {
+        user_choice = "rock";
+        system_choice = rps[Math.floor(Math.random() * rps.length)]
+        result = run_rps(user_choice,system_choice);
+        show_choices(user_choice, system_choice);
+        calculate_score(result);
+    });
+
+    /*Paper button handler*/
+    document.getElementById("Paper").addEventListener('click', function ()
+    {
+        user_choice = "paper";
+        system_choice = rps[Math.floor(Math.random() * rps.length)]
+        result = run_rps(user_choice,system_choice);
+        show_choices(user_choice, system_choice);
+        calculate_score(result);
+    });
+
+    /*Scissors button handler*/
+    document.getElementById("Scissors").addEventListener('click', function ()
+    {
+        user_choice = "scissors";
+        system_choice = rps[Math.floor(Math.random() * rps.length)]
+        result = run_rps(user_choice,system_choice);
+        show_choices(user_choice, system_choice);
+        calculate_score(result);
+    }); 
+
+    /*About me button handler */
+    document.getElementById("ABT_Button").addEventListener('click', function ()
+    {
+        document.getElementById("About_me_view").style.display = "block";
+        document.getElementById("Travel_list_view").style.display = "none";
+        document.getElementById("RPS_view").style.display = "none";
+        document.getElementById("WL_view").style.display = "none";
+    });
+
+    /*Travel list button handler */
+    document.getElementById("TVL_Button").addEventListener('click', function ()
+    {
+        document.getElementById("About_me_view").style.display = "none";
+        document.getElementById("Travel_list_view").style.display = "block";
+        document.getElementById("RPS_view").style.display = "none";
+        document.getElementById("WL_view").style.display = "none";
+    });
+
+    /*Rock paper scissors button handler */
+    document.getElementById("RPS_Button").addEventListener('click', function ()
+    {
+        document.getElementById("About_me_view").style.display = "none";
+        document.getElementById("Travel_list_view").style.display = "none";
+        document.getElementById("RPS_view").style.display = "block";
+        document.getElementById("WL_view").style.display = "none";
+    });
+
+    /*School Waitlist button handler */
+    document.getElementById("WL_Button").addEventListener('click', function ()
+    {
+        document.getElementById("About_me_view").style.display = "none";
+        document.getElementById("Travel_list_view").style.display = "none";
+        document.getElementById("RPS_view").style.display = "none";
+        document.getElementById("WL_view").style.display = "block";
+    });
+
+});
+
+/*determines result of game */
+function run_rps(choice1,choice2)
+{
+    if(choice1===choice2){
+        return "tie";
+    }
+    if(choice1==="rock"){
+        if(choice2==="scissors"){
+            return "user";
+        }
+        else{
+            return "system";
+        }
+    }
+    if(choice1==="paper"){
+        if(choice2==="rock"){
+            return "user";
+        }
+        else{
+            return "system";
+        }
+    }
+    if(choice1==="scissors"){
+        if(choice2==="rock"){
+            return "system";
+        }
+        else{
+            return "user";
+        }
+    }
+};
+
+/* calculates players' scores and show them */
+function calculate_score(result)
+{
+    if (result == "user")
+    {
+        user_score += 1;
+    }
+    else if(result == "system")
+    {
+        system_score += 1;
+    }
+
+    document.getElementById("User_score").innerHTML = "Your score is: " + user_score;
+    document.getElementById("System_score").innerHTML = "My score is: " + system_score;
+};
+
+/*show players choices*/
+function show_choices(user_choice, system_choice)
+{
+    document.getElementById("User_choice").innerHTML = "Your choice is: " + user_choice;
+    document.getElementById("System_choice").innerHTML = "My choice is: " + system_choice;
+};
+
+/*button handlers for school dropdown menu */
+function onClick_lincoln()
+{
+    document.getElementById("School_info").innerHTML = "School Info: Average gpa of admitted students is 3.00, Tech magnet program";
+}
+
+function onClick_xavier()
+{
+    document.getElementById("School_info").innerHTML = "School Info: Average gpa of admitted students is 3.3, Nationally ranked tennis program";
+}
+
+function onClick_hogwarts()
+{
+    document.getElementById("School_info").innerHTML = "School Info: Students generally perform wizardry";
+}
+
+/*This function will execute when the submit button is clicked */
+function formSubmit(theForm)
+{
+    document.getElementById("Submit_confirm").innerHTML = "Form submitted!";
+    theForm.preventDefault();
+}
+
